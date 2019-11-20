5e56fc67f5401529f86d304c1a8a53c7bb12887e
kedong <1150744690@qq.com>
2019-11-20 23:31:51 +0800

    add fxml

---
+++ src/main/java/com/doubledd/zpdhy/wallpaper/part3/FXMLExampleController.java
@@ -0,0 +1,13 @@
+package com.doubledd.zpdhy.wallpaper.part3;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.text.Text;
+
+public class FXMLExampleController {
+    @FXML public Text actiontarget;
+
+    @FXML protected void handleSubmitButtonAction(ActionEvent actionEvent) {
+        actiontarget.setText("Sign in button pressed");
+    }
+}
